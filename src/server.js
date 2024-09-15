1206e429ebbf5f8b7d58a5fcb149b776f931d9f4
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-15 21:01:41 +0300

    upd server, student.js, students.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import { env } from './utils/env.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -44,4 +44,32 @@ export const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      data: student,
+    });
+  });
+
+  app.get('/students', async (req, res) => {});
+
+  app.get('/students/:studentId', async (req, res) => {});
 };
