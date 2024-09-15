93e5dd62f91df7533231067a729ee681633cb2fd
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-15 10:54:47 +0300

    install pino, pino-pretty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-const message = "Hello world";
+import express from 'express';
 
-console.log(message);
+const PORT = 3000;
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.use(express.json());
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
