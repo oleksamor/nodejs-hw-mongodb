6e864597799b25af0d942945cda20b12a630fd00
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-15 16:10:25 +0300

    method connect add, create bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { initMongoDB } from './db/initMongoDB.js';
 import { startServer } from './server.js';
 
-startServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+bootstrap();
