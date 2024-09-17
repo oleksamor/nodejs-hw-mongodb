b85f1aa4ffbfa08fefae452c7f0467624b890afb
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-17 18:02:49 +0300

    add  status: 200,

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -41,6 +42,7 @@ export const setupServer = () => {
       return;
     }
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
