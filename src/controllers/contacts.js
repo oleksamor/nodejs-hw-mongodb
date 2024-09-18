73f683a7c93f7813577b6829774bc9d8b24877e5
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-18 12:05:38 +0300

    correct error 404

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res, next) => {
@@ -20,9 +21,9 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    next(new Error('Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
+
   res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,14 @@
+import { HttpError } from 'http-errors';
+
 export const errorHadler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+  }
+
   res.status(500).json({
     message: 'Something went wrong',
     error: err.message,
