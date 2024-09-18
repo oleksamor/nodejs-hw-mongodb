e303bc0ade6355b3cb1025cf08d1e74a4cf23d4c
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-18 10:29:51 +0300

    add try catch

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,13 +1,17 @@
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
 
-  res.status(200).json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getContactByIdController = async (req, res, next) => {
@@ -16,9 +20,7 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
+    next(new Error('Contact not found'));
     return;
   }
   res.status(200).json({
