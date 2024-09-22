8ce802613ed86c5b04994498eebfa0de976129e0
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-09-22 21:07:20 +0300

    add validateBody(createContactSchema),

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,33 +9,47 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
-
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+// createContactSchema
 const contactsRouter = Router();
 
-contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
+contactsRouter.get(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(getContactsController),
+);
 
 contactsRouter.get(
   '/contacts/:contactId',
+  validateBody(createContactSchema),
   isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
-contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+contactsRouter.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
 contactsRouter.delete(
   '/contacts/:contactId',
+  validateBody(createContactSchema),
   isValidId,
   ctrlWrapper(deleteContactController),
 );
 
 contactsRouter.put(
   '/contacts/:contactId',
+  validateBody(createContactSchema),
   isValidId,
   ctrlWrapper(putContactController),
 );
 
 contactsRouter.patch(
   '/contacts/:contactId',
+  validateBody(createContactSchema),
   isValidId,
   ctrlWrapper(patchContactsController),
 );
