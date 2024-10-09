5929b7c641f2882bd48b63b4af3197774df02c31
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-09 16:06:33 +0300

    create two cookies

--- src/controllers/auts.js
+++ src/controllers/auts.js
@@ -1,5 +1,6 @@
 import { loginUser, registerUser } from '../services/auth.js';
 import { serializedUser } from '../utils/serializedUser.js';
+import { ONE_DAY } from '../constants/index.js';
 
 export const registerController = async (req, res) => {
   const { body } = req;
@@ -16,10 +17,22 @@ export const registerController = async (req, res) => {
 export const loginUserController = async (req, res) => {
   const { body } = req;
 
-  const user = await loginUser(body);
+  const session = await loginUser(body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
 
   res.json({
     status: 200,
     message: 'Successfully logged in a user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
