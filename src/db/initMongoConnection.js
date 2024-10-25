ad1f54085350b0b79b6eea7360ec64906e9a157c
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-25 14:34:38 +0300

    change  db/initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,7 +6,7 @@ export const initMongoConnection = async () => {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
-    const db = 'MONGODB_DB';
+    const db = env('MONGODB_DB');
 
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority `,
