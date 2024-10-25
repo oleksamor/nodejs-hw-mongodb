a6897f45ea4280dbd0d3df37cf3334c1bb373371
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-25 12:43:42 +0300

    add env.exsample, env JWT_SECRET, await sendEmail

--- src/services/auth.js
+++ src/services/auth.js
@@ -97,6 +97,22 @@ export const requestResetToken = async (email) => {
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
 
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
   //доповнимо її трохи пізніше
 };
