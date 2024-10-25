f5fb1fbcc7fa1800f5afc74908b39faa46cf2a63
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-25 12:16:01 +0300

    add requestResetToken

--- src/services/auth.js
+++ src/services/auth.js
@@ -91,3 +91,12 @@ export const refreshSession = async (sessionId, sessionToken) => {
   });
   return newSession;
 };
+
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  //доповнимо її трохи пізніше
+};
