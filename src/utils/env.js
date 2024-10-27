e79e51f90c93f058831b34139fb5fce1e74c0de8
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-27 13:45:47 +0200

    change env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function env(name, defaultValue) {
-  const value = process.env[name];
+  const value = process.env[`${name}`];
 
   if (value) return value;
 
   if (defaultValue) return defaultValue;
 
-  throw new Error(`Missing: process.env['${name}'].`);
+  throw new Error(`Missing: process.env[${name}]`);
 }
