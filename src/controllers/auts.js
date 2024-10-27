295d6ba2d53d893a417734dada4da73ca035b106
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-10-27 21:56:09 +0200

    add google-oauth.json, upd controller auth

--- src/controllers/auts.js
+++ src/controllers/auts.js
@@ -102,7 +102,7 @@ export const getGoogleOAuthUrlController = async (req, res) => {
 
 export const loginWithGoogleController = async (req, res) => {
   const session = await loginOrSignupWithGoogle(req.body.code);
-  setupSession(res, session);
+  setupSessionCookies(session, res);
 
   res.json({
     status: 200,
