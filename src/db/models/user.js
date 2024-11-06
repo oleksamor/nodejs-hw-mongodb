ce9184e89fc729bba32df6fb43d2b778beeecc71
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-11-06 18:59:44 +0200

    upd models user

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,5 @@
 import { model, Schema } from 'mongoose';
-import { ROLES } from '../../constants/index.js';
+// import { ROLES } from '../../constants/index.js';
 
 const userSchema = new Schema(
   {
